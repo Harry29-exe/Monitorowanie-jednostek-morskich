a236c83b59e16b0ea593893eb2f3f5f396e48336
Kamil Wójcik <wojcikakamil@gmail.com>
2021-11-29 13:40:49 +0100

    added security authentication

--- backend/src/main/java/com/example/monitorowaniejednostekmorskich/config/SecurityConfig.java
+++ backend/src/main/java/com/example/monitorowaniejednostekmorskich/config/SecurityConfig.java
@@ -1,9 +1,25 @@
 package com.example.monitorowaniejednostekmorskich.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final AuthenticationProvider authProvider;
+
+    public SecurityConfig(PasswordEncoder encoder, UserDetailsService userDetailsService) {
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setPasswordEncoder(encoder);
+        authProvider.setUserDetailsService(userDetailsService);
+        this.authProvider = authProvider;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -12,5 +28,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .disable()
             .csrf()
                 .disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(this.authProvider);
+    }
+
+    @Bean
+    public AuthenticationManager getAuthenticationManager() throws Exception {
+        return this.authenticationManager();
     }
 }
